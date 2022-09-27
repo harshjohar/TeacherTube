a20c201b2f66da487fbf7992e8e77b6b442fec22
Harasees Singh <76965050+harasees-singh@users.noreply.github.com>
2022-09-27 16:40:32 +0530

    routes working, dashboard grid done

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -1,9 +1,39 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import ClassTile from '../components/classroom/ClassTile'
+import styles from './Dashboard.module.css'
+import Classroom from './Classroom'
+import Class from '../models/class'
 
 function Dashboard() {
-  return (
-    <div>Dashboard</div>
-  )
+    const classesList: Class[] = [
+        new Class('software engineering', 'rajesh bhatia'),
+        new Class('machine learning', 'padmavati'),
+        new Class('machine learning', 'padmavati'),
+        new Class('machine learning', 'padmavati'),
+        new Class('machine learning', 'padmavati'),
+        new Class('machine learning', 'padmavati'),
+        new Class('machine learning', 'padmavati'),
+    ]
+    return (
+        <>
+            <Switch>
+                <Route path='/dashboard' exact>
+                    {/* grid of classtiles */}
+                    <div className={styles.grid}>
+                        {
+                            classesList.map( (classObject) => {
+                                return <ClassTile {...classObject} />
+                            })
+                        }
+                    </div>
+                </Route>
+                <Route path='/dashboard/class'>
+                    <Classroom />
+                </Route>
+            </Switch>
+        </>
+    )
 }
 
 export default Dashboard
